5dab57babad23c4c90d7094308f90e87e664bdc5
ngarng00 <71837697+ngarng00@users.noreply.github.com>
2025-03-03 16:09:29 +0100

    Ejercicos 1 y 2 Practica 1

--- src/sis_2/Sis_2.java
+++ src/sis_2/Sis_2.java
@@ -4,58 +4,71 @@
  * and open the template in the editor.
  */
 package sis_2;
-
-
-import java.util.Scanner;
-import POJOS.*;
-import java.util.List;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.SessionFactory;
 /**
  *
- * @author gar27
+ * @author Nicol
  */
-public class Sis_2 {
+
+import POJOS.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class sis_2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Introduzca un DNI:");
-        String newNIF = sc.nextLine();
-        
-        
         SessionFactory sf = null;
-        Session session = null;
+        Session sesion = null;
         Transaction tx = null;
-        
+  
+        System.out.println("INTRODUZCA SU DNI:");
+        Scanner sc = new Scanner (System.in);
+        String DNI = sc.nextLine();
+  
         try{
             sf = HibernateUtil.getSessionFactory();
-            session = sf.openSession();
+            sesion = sf.openSession();
             
-            String HQL = "from Recibos r WHERE Recibos.idContribuyente.nifnie = :nifNie";
-            
-            Query query = session.createQuery(HQL);
-            query.setParameter("nifNie", newNIF);
-            
-            List<Contribuyente> lista = query.list();
+            Query queryC = sesion.createQuery("SELECT c FROM Contribuyente c WHERE c.nifnie = :n");
+            queryC.setParameter("n", DNI);
             
             
+            
+            Contribuyente c = (Contribuyente) queryC.uniqueResult();
+            System.out.println(c.getNombre());
+            System.out.println(c.getApellido1());
+            System.out.println(c.getApellido2());
+            System.out.println(c.getNifnie());
+            System.out.println(c.getDireccion());
+            
+
+            Query queryR = sesion.createQuery("SELECT r FROM Recibos r WHERE r.nifContribuyente = :n");
+            queryR.setParameter("n", DNI);
+            
+            List<Recibos> listaRecibos = queryR.list();
+            tx = sesion.beginTransaction();
+            for (Recibos r : listaRecibos) {
+                System.out.println(r.getTotalRecibo());
+                r.setTotalRecibo(115D);
+                System.out.println(r.getTotalRecibo());
+                sesion.update(r);
+            }
+            tx.commit();
+            
+            sesion.close();
+            sf.close();
+        }finally{
             
         }
-        catch(NullPointerException e){
-            System.out.println("El usuario no se encuentra entre nuestros datos."); 
-        }
-        
-        sc.close();
-        
-        
+           
     }
-    
+    //09677930J
 }
