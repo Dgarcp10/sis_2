5a5521b13f9d932f3482b26a75b9707a94d5b08a
ngarng00 <ngarng00@estudiantes.unileon.es>
2025-03-12 17:55:10 +0100

    Comienzo Arreglos y rama HotFix

---
+++ src/sis_2/Conexion.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sis_2;
+
+import POJOS.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author Nicol
+ */
+public class Conexion {
+    private static Conexion instancia;
+    private SessionFactory sessionFactory;
+
+    private Conexion(){
+        sessionFactory = HibernateUtil.getSessionFactory();
+    }
+    
+    public static Conexion getIntance(){
+        if(instancia == null){
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+    
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+}
