7c770a1f1decbe4c7d92acc701c3a9afdc4d8c7a
Diego <dgarcp10@estudiantes.unileon.es>
2025-03-21 12:32:32 +0100

    Fuera comentarios y datos de prueba

--- src/sis_2/XmlManager.java
+++ src/sis_2/XmlManager.java
@@ -49,81 +49,50 @@ public class XmlManager {
             String rutaArchivo = "resources/";
             String nombreArchivo = "ErroresNifNie.xml";
             archivo = new File(rutaArchivo + nombreArchivo);
-            
-            //System.out.println("A");
+
         } catch (ParserConfigurationException ex) {
             System.out.println("ERROR: no se pudo abrir/crear el ErroresNifNie.xml correctamente");
         }
-        
-// Esto es una prueba y funciona el fallo esta en el agregar
-        Element contribuyente = documento.createElement("Contribuyente");
-        contribuyente.setAttribute("id", String.valueOf(0));
-        rootElem.appendChild(contribuyente);
-        
-        
-        Element NIF_NIE = documento.createElement("NIF_NIE");
-        //Text textNIF_NIE = documento.createTextNode("71314712S");
-        //NIF_NIE.appendChild(textNIF_NIE);
-        
-        NIF_NIE.appendChild(documento.createTextNode(""));
-        contribuyente.appendChild(NIF_NIE); 
-
-        Element Nombre = documento.createElement("Nombre");
-        Text textNombre = documento.createTextNode("Diego");
-        Nombre.appendChild(textNombre);
-        contribuyente.appendChild(Nombre);
-
-        Element PrimerApellido = documento.createElement("PrimerApellido");
-        Text textPrimerApellido = documento.createTextNode("García");
-        PrimerApellido.appendChild(textPrimerApellido);
-        contribuyente.appendChild(PrimerApellido);
-        
-        
-        
-        
-// hasta aqui llega la prueba
     }
     
     public void agregarContribuyente(Contribuyente con){
-        Element contribuyente = documento.createElement("Contribuyente");
-        contribuyente.setAttribute("id", String.valueOf(con.getIdExcel()));
-        rootElem.appendChild(contribuyente);
-        
-        Element NIF_NIE = documento.createElement("NIF_NIE");
-        Text textNIF_NIE;
-        if (!"".equals(con.getNifnie()) && con.getNifnie() != null) {
-            textNIF_NIE = documento.createTextNode(con.getNifnie());
-            NIF_NIE.appendChild(textNIF_NIE);
-        }else{
-            textNIF_NIE = documento.createTextNode("");
-            NIF_NIE.appendChild(textNIF_NIE);
-        }
-        
-        System.out.println("\nMi dni es: " + textNIF_NIE);
-        contribuyente.appendChild(NIF_NIE); 
-
-        Element Nombre = documento.createElement("Nombre");
-        Text textNombre = documento.createTextNode(con.getNombre());
-        System.out.println("Mi nombre es: " +con.getNombre());
-        Nombre.appendChild(textNombre);
-        contribuyente.appendChild(Nombre);
-
-        Element PrimerApellido = documento.createElement("PrimerApellido");
-        Text textPrimerApellido = documento.createTextNode(con.getApellido1());
-        System.out.println("Mi apellido es: " +con.getApellido1());
-        PrimerApellido.appendChild(textPrimerApellido);
-        contribuyente.appendChild(PrimerApellido);
-
-        if (con.getApellido2() != null && !con.getApellido2().isEmpty()) {
-            Element SegundoApellido = documento.createElement("SegundoApellido");
-            Text textSegundoApellido = documento.createTextNode(con.getApellido2());
-            SegundoApellido.appendChild(textSegundoApellido);
-            contribuyente.appendChild(SegundoApellido);
-        }
-        
-        
         if(con.getErrNif() == null || "".equals(con.getErrNif())){
         }else{
+            
+            Element contribuyente = documento.createElement("Contribuyente");
+            contribuyente.setAttribute("id", String.valueOf(con.getIdExcel()));
+            rootElem.appendChild(contribuyente);
+
+            Element NIF_NIE = documento.createElement("NIF_NIE");
+            Text textNIF_NIE;
+            if (con.getNifnie() == null) {
+                textNIF_NIE = documento.createTextNode("");
+                NIF_NIE.appendChild(textNIF_NIE);
+            }else{
+                textNIF_NIE = documento.createTextNode(con.getNifnie());
+                NIF_NIE.appendChild(textNIF_NIE);
+            }
+            contribuyente.appendChild(NIF_NIE); 
+
+            Element Nombre = documento.createElement("Nombre");
+            Text textNombre = documento.createTextNode(con.getNombre());
+            Nombre.appendChild(textNombre);
+            contribuyente.appendChild(Nombre);
+
+            Element PrimerApellido = documento.createElement("PrimerApellido");
+            Text textPrimerApellido = documento.createTextNode(con.getApellido1());
+            PrimerApellido.appendChild(textPrimerApellido);
+            contribuyente.appendChild(PrimerApellido);
+
+            if (con.getApellido2() != null && !con.getApellido2().isEmpty()) {
+                Element SegundoApellido = documento.createElement("SegundoApellido");
+                Text textSegundoApellido = documento.createTextNode(con.getApellido2());
+                SegundoApellido.appendChild(textSegundoApellido);
+                contribuyente.appendChild(SegundoApellido);
+            }
+        
+        
+        
             Element TipoDeError = documento.createElement("TipoDeError");
             Text textTipoDeError = documento.createTextNode(con.getErrNif());
             TipoDeError.appendChild(textTipoDeError);
@@ -138,18 +107,12 @@ public class XmlManager {
             System.out.println(rootElem);
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
-            
-            //Prueba 2
             transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            //Fin prueba 2
             
             DOMSource source = new DOMSource(documento);
-            //System.out.println(archivo.getAbsolutePath());
             StreamResult result = new StreamResult(archivo);
-            //if(documento != null) System.out.println("bla");
             transformer.transform(source, result);
-            
             
             salida = true;
         } catch (TransformerConfigurationException ex) {
@@ -160,15 +123,4 @@ public class XmlManager {
         }
         return salida;
     }
-    
-    /*
-    <Contribuyente id="2">
-    <NIF_NIE>125C</NIF_NIE>
-    <Nombre>Juan</Nombre>
-    <PrimerApellido>Martinez</PrimerApellido>
-    <SegundoApellido>Dominguez</SegundoApellido>
-    <TipoDeError>NIF ERRONEO</TipoDeError>
-    </Contribuyente>
-    */
-
 }
