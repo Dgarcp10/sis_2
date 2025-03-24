a8c59cac342bca6cb15e81e2b8cf40336057250a
Diego <dgarcp10@estudiantes.unileon.es>
2025-03-24 17:17:25 +0100

    inicio pract 3

--- src/sis_2/Utilities.java
+++ src/sis_2/Utilities.java
@@ -151,4 +151,75 @@ public class Utilities {
         System.arraycopy(listaNIFNIE, 0, nuevoArray, 0, listaNIFNIE.length);    // Copiar los elementos del array original al nuevo array
         listaNIFNIE = nuevoArray;   // Asignar el nuevo array al array original.
     }
+    
+    /**
+     * 
+     * @param con
+     * @return contribuyente actualizado.
+     */
+    public Contribuyente validadorCCC(Contribuyente con){
+        String ccc = con.getCcc();
+        String regexp = "^[\\d]{20}$";
+        if(Pattern.matches(regexp, ccc)){
+            //Tiene 20 caracteres numericos 
+            
+            
+        }else{
+            //Es un ccc que no cumple la forma, es erroneo, se manda al xml de errores.
+            con.setErrCcc("IMPOSIBLE GENERAR IBAN");
+        }
+        
+        return con;
+    }
+    
+    private void corregirCCC(String ccc) {
+        int[] parte1 = new int[10];
+        int[] parte2 = new int[10];
+        int[] control =new int[2];
+        parte1[0] = 0;
+        parte1[1] = 0;
+        for (int i = 0; i < ccc.length(); i++) {
+            if(i < 8){
+                parte1[i+2] = Character.getNumericValue(ccc.charAt(i));
+            }else if(i < 10){
+                control[i-8] = Character.getNumericValue(ccc.charAt(i));
+            }else{
+                parte2[i-10] = Character.getNumericValue(ccc.charAt(i));
+            }
+        }
+        int aux1 = digitoControl(parte1);
+        int aux2 = digitoControl(parte2);
+        if(control[0] == aux1 && control[1] == aux2){
+            // CCC correcto salimos
+            
+        }else 
+        StringBuilder sb = new StringBuilder(); 
+        for(int i=2; i<parte1.length; i++){
+            sb.append(parte1[i]);
+        }
+        sb.append(control[0]);
+        sb.append(control[1]);
+        for(int i=0; i<parte2.length; i++){
+            sb.append(parte2[i]);
+        }
+        return sb.toString();
+    }
+    
+    /**
+     * 
+     * @param substring
+     * @return 
+     */
+    public int digitoControl(int[] substring){
+        int[] multi = {1,2,4,8,5,10,9,7,3,6};
+        int control = 0;
+        for (int i = 0; i < 10; i++) {
+            control += multi[i] * substring[i];
+        }
+        control = 11 - (control % 11);
+        if(control == 10) control = 1;
+        if(control == 11) control = 0;
+        return control;
+    }
+    
 }
